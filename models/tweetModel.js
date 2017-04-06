43034b2dfc0b40d47fce5702afdf8467d596edda
root <root@twitter-backend-2.cloud.compas.cs.stonybrook.edu>
2017-04-06 12:23:38 +0000

    fixing indexes

--- models/tweetModel.js
+++ models/tweetModel.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 var tweetSchema = mongoose.Schema({
-	content: {type: String, index:true}
+	content: {type: String, index:true},
 	id: {type: String, index:true},
-	username: {type: String, index:true}
+	username: {type: String, index:true},
 	parent: String,
 	timestamp: {type: Number, index:true}
 });
