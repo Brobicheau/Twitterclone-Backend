ffb47b39a4963dd21fc34f917b7f48bed7e31ecf
root <root@application-server-1.cloud.compas.cs.stonybrook.edu>
2017-04-22 20:54:56 +0000

    here

--- models/tweetModel.js
+++ models/tweetModel.js
@@ -6,10 +6,10 @@ var tweetSchema = mongoose.Schema({
 	content: String,
 	id: {type: String, unique:true},
 	username: {type: String, index: true},
-	parent:{type: String, index: true},
-	timestamp: {type: Number, index: true},
+	parent:{type: String},
+	timestamp: {type: Number},
 	media: [String]
 });
-tweetSchema.index({content: 'text'});
+//tweetSchema.index({content: 'text'});
 
 module.exports = conn1.model('tweets', tweetSchema);
